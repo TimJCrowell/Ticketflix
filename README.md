836d41f73a26104666905f98a5c7e71843932c2e
TimJCrowell <timothy.crowell.630@my.csun.edu>
2026-03-12 03:48:14 -0700

    Hello World skeleton codebase with proposed technologies

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ticketflix
---
+++ src/main/java/com/moviebooking/HelloController.java
@@ -0,0 +1,13 @@
+package com.moviebooking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public Map<String, String> sayHello() {
+        return Map.of("message", "Hello from Java 21 Backend!");
+    }
+}
