6f3d8f08a1ffc5d1b1ba849963183d81b2337278
Josei <j0125rio@gmail.com>
2026-04-11 16:24:58 -0700

    starting checkout class implementation

---
+++ src/main/java/com/moviebooking/entity/Checkout.java
@@ -0,0 +1,9 @@
+package com.moviebooking.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
