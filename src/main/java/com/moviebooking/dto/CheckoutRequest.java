cc4f138b6acd2fd2cbbe1821049fd44eb008d24e
Josei <j0125rio@gmail.com>
2026-04-11 17:37:10 -0700

    Finished CheckoutRequest class, will look through for any errors

---
+++ src/main/java/com/moviebooking/dto/CheckoutRequest.java
@@ -0,0 +1,29 @@
+package com.moviebooking.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CheckoutRequest
+{
+    private long userId;
+    private long showtimeId;
+
+    /*----------------------------------------------------------------------------------------------------
+    place holder until we have a seat class
+    */
+
+    private List<String> seatLabels;
+    private BigDecimal clientTotal;
+
+    public long getUserId() {return userId}
+    public void setUserId(Long userId) {this.userId = userId;}
+
+    public long getShowtimeId() {return showtimeId}
+    public void setShowtimeId(Long showtimeId) {this.showtimeId = showtimeId;}
+
+    private List<String> getSeatLabels() {return seatLabels}
+    public void setSeatLabels(List<String> seatLabels) {this.seatLabels = seatLabels;}
+
+    public BigDecimal getClientTotal() {return clientTotal}
+    public void setClientTotal(BigDecimal clientTotal) {this.clientTotal = clientTotal;}
+}//end of CheckoutRequest class
