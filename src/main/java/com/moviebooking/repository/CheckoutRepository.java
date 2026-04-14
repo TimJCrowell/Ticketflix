fc7b7bd0bfe69da573a1d593ef49ce915e1811cd
Josei <j0125rio@gmail.com>
2026-04-13 19:21:53 -0700

    completed checkoutrepository

---
+++ src/main/java/com/moviebooking/repository/CheckoutRepository.java
@@ -0,0 +1,5 @@
+package com.moviebokking.reporsitory;
+import com.moviebooking.entity.Checkout;
+import org.springframwork.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long>{}
