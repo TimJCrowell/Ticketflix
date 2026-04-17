5fa735e9e5d4c401307e41839be6f391c0d43aff
Josei <j0125rio@gmail.com>
2026-04-17 13:58:41 -0700

    finished java docing for this file.

--- src/main/java/com/moviebooking/repository/CheckoutRepository.java
+++ src/main/java/com/moviebooking/repository/CheckoutRepository.java
@@ -2,4 +2,7 @@ package com.moviebooking.repository;
 import com.moviebooking.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for CRUD access to checkout entities.
+ */
 public interface CheckoutRepository extends JpaRepository<Checkout, Long>{}
