4a9ad22934781ae6b2692a902f96e4b9013a76ca
Josei <j0125rio@gmail.com>
2026-04-17 14:06:35 -0700

    finished java doc for this file, have finished all commenting on all of my files now

--- src/main/java/com/moviebooking/controller/CheckoutController.java
+++ src/main/java/com/moviebooking/controller/CheckoutController.java
@@ -8,13 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * REST controller for checkout related endpoints.
+ */
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController
 {
+    /** Service dependency which handles checkout buisness logic.  */
     @Autowired
     private CheckoutService checkoutService;
 
+    /**
+     * Creates checkout with client requested data.
+     * @param request icoming checkout payload.
+     * @return HTTP 202 response with created checkout.
+     */
     @PostMapping
     public ResponseEntity<CheckoutResponse> checkout(@RequestBody CheckoutRequest request)
     {
