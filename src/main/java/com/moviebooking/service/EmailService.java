f7c9fe51c2e0d78bb7029b6d8d2bf5d3e617066b
Josei <j0125rio@gmail.com>
2026-04-24 20:35:04 -0700

    starting email confirmation implementation

---
+++ src/main/java/com/moviebooking/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.moviebooking.service;
+
+import com.moviebooking.entity.Checkout;
+import com.moviebooking.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
