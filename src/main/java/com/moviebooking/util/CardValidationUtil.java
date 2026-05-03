2a71c79516b05fddd1cfd600f72d882d4f72ee8b
Josei <j0125rio@gmail.com>
2026-05-02 21:39:31 -0700

    Starting cardvalidation implementation

---
+++ src/main/java/com/moviebooking/util/CardValidationUtil.java
@@ -0,0 +1,8 @@
+package com.moviebooking.util;
+
+/**
+ * Local card number checks only (Luhn / length). No payment network, no storage.
+ */
+public final CardValidationUtil{
+
+}//end CardValidaiton.java class
